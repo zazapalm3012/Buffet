6636a53befdeb67727f1efba55fb8d8740632f81
NousagiP <tirapatthettean@gmail.com>
2024-03-20 19:50:17 +0700

    HI just pull from palm

---
+++ Buffet/Controllers/RestaurantController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buffet.Controllers
+{
+	public class RestaurantController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
