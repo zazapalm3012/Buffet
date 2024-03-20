6ef5b0318956efc90bcb4fef9544d598a45cee0f
zazapalm3012 <zazapalm3012@hotmail.com>
2024-03-20 19:49:25 +0700

    ResController and View

---
+++ Buffet/Controllers/RestaurantController.cs
@@ -0,0 +1,31 @@
+﻿using Buffet.Models;
+using Buffet.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Buffet.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private readonly BuffetContext _db;
+
+        public RestaurantController(BuffetContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Total()
+        {
+            return View();
+        }
+    }
+}
