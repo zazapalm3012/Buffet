f493c8f98781b9fec2f2521bac987bc708cf7551
Notegit <157777719+gitnotedotcom@users.noreply.github.com>
2024-03-30 23:08:33 +0700

    Homepage

--- Buffet/wwwroot/js/site.js
+++ Buffet/wwwroot/js/site.js
@@ -1,4 +1,102 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+        "#recomend"
+    );
+    if (window.matchMedia("(min-width: 576px)").matches) {
+        var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+            interval: false,
+            wrap: false
+        });
+        var carouselWidth = $(".menu-inner")[0].scrollWidth;
+        var cardWidth = $(".menu-item").width();
+        var scrollPosition = 0;
+        $("#recomend .next-icon").on("click", function () {
+            if (scrollPosition < carouselWidth - cardWidth * 3) {
+                scrollPosition += cardWidth;
+                $("#recomend .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+        $("#recomend .prev-icon").on("click", function () {
+            if (scrollPosition > 1) {
+                scrollPosition -= cardWidth;
+                $("#recomend .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+    } else {
+        $(multipleCardCarousel).addClass("slide");
+    }
+});
+$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+        "#foodtype"
+    );
+    if (window.matchMedia("(min-width: 576px)").matches) {
+        var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+            interval: false,
+            wrap: false
+        });
+        var carouselWidth = $(".menu-inner")[0].scrollWidth;
+        var cardWidth = $(".menu-item").width();
+        var scrollPosition = 0;
+        $("#foodtype .next-icon").on("click", function () {
+            if (scrollPosition < carouselWidth - cardWidth * 3) {
+                scrollPosition += cardWidth;
+                $("#foodtype .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+        $("#foodtype .prev-icon").on("click", function () {
+            if (scrollPosition > 1) {
+                scrollPosition -= cardWidth;
+                $("#foodtype .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+    } else {
+        $(multipleCardCarousel).addClass("slide");
+    }
+});
+$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+        "#DiningStyle"
+    );
+    if (window.matchMedia("(min-width: 576px)").matches) {
+        var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+            interval: false,
+            wrap: false
+        });
+        var carouselWidth = $(".menu-inner")[0].scrollWidth;
+        var cardWidth = $(".menu-item").width();
+        var scrollPosition = 0;
+        $("#DiningStyle .next-icon").on("click", function () {
+            if (scrollPosition < carouselWidth - cardWidth * 3) {
+                scrollPosition += cardWidth;
+                $("#DiningStyle .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+        $("#DiningStyle .prev-icon").on("click", function () {
+            if (scrollPosition > 1) {
+                scrollPosition -= cardWidth;
+                $("#DiningStyle .menu-inner").animate(
+                    { scrollLeft: scrollPosition },
+                    600
+                );
+            }
+        });
+    } else {
+        $(multipleCardCarousel).addClass("slide");
+    }
+});
