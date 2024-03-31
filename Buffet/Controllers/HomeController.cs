9bd2114ee3c1c23d4108ce0bffbc20c2a8e142ed
Notegit <157777719+gitnotedotcom@users.noreply.github.com>
2024-03-31 18:35:18 +0700

    payment , booking , privacy

--- Buffet/Controllers/HomeController.cs
+++ Buffet/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace Buffet.Controllers
         {
             return View();
         }
+        public IActionResult Booking()
+        {
+            return View();
+        }
+        public IActionResult Payment()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
